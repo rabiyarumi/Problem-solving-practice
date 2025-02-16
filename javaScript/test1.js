fcd490e6d4188d587d9a636dff21fb902dba1701
rabiyarumi <rabiyaarumi@gmail.com>
2025-02-16 15:06:30 +0600

    added sum of array practice

--- javaScript/test1.js
+++ javaScript/test1.js
@@ -1,13 +1,13 @@
 //reverse a string with build in function
 
 function reverseString(str) {
-    console.log(str)
+    // console.log(str)
     let splitStr = str.split("")
-    console.log(splitStr)
+    // console.log(splitStr)
     let reverseSplitStr = splitStr.reverse()
-    console.log(reverseSplitStr)
+    // console.log(reverseSplitStr)
     let joinReversedStr= reverseSplitStr.join("")
-    console.log(joinReversedStr)
+    // console.log(joinReversedStr)
     return joinReversedStr
 
     //shortcut
@@ -22,8 +22,31 @@ function reverseString2(str){
     for(let i = str.length - 1; i >= 0; i--){
         newStr += str[i]
     }
-    console.log(newStr)
+    // console.log(newStr)
     return newStr
 }
 
 reverseString2("hello")
+
+
+//sum of array
+function sumOfArray(){
+    function readLine() {
+        return inputString[currentLine++];
+    }
+    const n = parseInt(readLine().trim(), 10);
+    
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    
+    console.log(arr)
+
+    let sum = 0;
+    for(const a of arr){
+        console.log(a)
+        sum = sum + a
+    }
+    console.log(sum)
+    return sum
+}
+sumOfArray("4 6 8 9 5 3 2")
+
