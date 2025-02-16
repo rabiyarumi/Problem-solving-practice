ac8475262c04388c5af4eabfa26fcb7ee822a86e
rabiyarumi <rabiyaarumi@gmail.com>
2025-02-16 17:35:08 +0600

    Solve Find the maximam number

--- javaScript/test1.js
+++ javaScript/test1.js
@@ -1,65 +1,78 @@
 //reverse a string with build in function
 
 function reverseString(str) {
-    // console.log(str)
-    let splitStr = str.split("")
-    // console.log(splitStr)
-    let reverseSplitStr = splitStr.reverse()
-    // console.log(reverseSplitStr)
-    let joinReversedStr= reverseSplitStr.join("")
-    // console.log(joinReversedStr)
-    return joinReversedStr
+  // console.log(str)
+  let splitStr = str.split("");
+  // console.log(splitStr)
+  let reverseSplitStr = splitStr.reverse();
+  // console.log(reverseSplitStr)
+  let joinReversedStr = reverseSplitStr.join("");
+  // console.log(joinReversedStr)
+  return joinReversedStr;
 
-    //shortcut
-    // return str.split("").reverse().join("");
+  //shortcut
+  // return str.split("").reverse().join("");
 }
 
-reverseString("Sidra")
+reverseString("Sidra");
 
 //reverse a string with Decrementing for loop
-function reverseString2(str){
-    let newStr=""
-    for(let i = str.length - 1; i >= 0; i--){
-        newStr += str[i]
-    }
-    // console.log(newStr)
-    return newStr
+function reverseString2(str) {
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  // console.log(newStr)
+  return newStr;
 }
 
-reverseString2("hello")
-
+reverseString2("hello");
 
 //sum of array
-function sumOfArray(arr){
-    // console.log(arr)
-    let sum = 0;
-    for(const a of arr){
-        // console.log(a)
-        sum = sum + a
-    }
-    // console.log(sum)
-    return sum
+function sumOfArray(arr) {
+  // console.log(arr)
+  let sum = 0;
+  for (const a of arr) {
+    // console.log(a)
+    sum = sum + a;
+  }
+  // console.log(sum)
+  return sum;
 }
-sumOfArray([6,10,3,6,9,6])
-
+sumOfArray([6, 10, 3, 6, 9, 6]);
 
 // Find the even and odd number
 
-function checkParity(n){
-// console.log(n)
-if(n % 2 === 0 && n >= 0){
-    console.log("Even")
+function checkParity(n) {
+  // console.log(n)
+  if (n % 2 === 0 && n >= 0) {
+    // console.log("Even");
+  } else if (n % 2 === 1 && n >= 0) {
+    // console.log("Odd");
+  } else if (n < 0) {
+    // console.log("Number is negative");
+  }
 }
-else if(n % 2 === 1 && n >= 0){
-    console.log("Odd")
+checkParity(-999);
+
+
+function findTheMax(arr){
+    // console.log(arr)
+    let max = arr[0];
+    for (const a of arr){
+        if(a > max){
+            max = a
+        }
+    }
+    console.log(max)
+    return max
 }
-else if(n < 0){
-    console.log("Number is negative")
+
+findTheMax([ -5, 0, -1, -9, -2])
+
+
+function findTheMax2(arr) {
+    return Math.max(...arr);
 }
 
-
-}
-
-checkParity(-999)
-
-
+// console.log(findTheMax2([-5, -7, -1, -9, -2])); 
